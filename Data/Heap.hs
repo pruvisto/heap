83e554779982e4a799586d4a8bc76a9434bc627a
Robert Soeldner <r.soeldner@gmail.com>
2018-03-28 09:03:11 +0200

    Add updateValue

--- Data/Heap.hs
+++ Data/Heap.hs
@@ -49,6 +49,9 @@ module Data.Heap
       -- ** Ordered list
     , fromAscList, toAscList
     , fromDescList, toDescList
+
+     -- ** Update value
+    , updateValue
     ) where
 
 import Data.Heap.Item
@@ -166,3 +169,7 @@ fromDescList = I.fromDescList . fmap split
 -- efficient 'fromDescList' function.
 toDescList :: (HeapItem pol item) => Heap pol item -> [item]
 toDescList = reverse . toAscList
+
+-- | /O(n log n)/. Update the value of a specific priority.
+updateValue :: Ord prio => HeapT prio val -> prio -> val -> HeapT prio val
+updateValue = I.updateValue
